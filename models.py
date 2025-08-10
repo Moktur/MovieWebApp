fa57de164182b3bbf86b25452ea399f19bc2ad61
pepe <p.zajonz@posteo.de>
2025-08-10 17:41:05 +0200

    added uniqueness for names

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ class User(db.Model):
     """
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False, unique=True)
 
 class Movie(db.Model):
     """
