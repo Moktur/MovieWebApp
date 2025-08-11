762962823041d53d075130899ba41992ecd41ff0
pepe <p.zajonz@posteo.de>
2025-08-11 06:19:18 +0200

    changed unique preferences in movieclass

--- models.py
+++ models.py
@@ -36,8 +36,8 @@ class Movie(db.Model):
     __tablename__ = 'movies'
     # TODO evt unique=True bei name, director rausmachen
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
-    director = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    director = db.Column(db.String(100), unique=False, nullable=False)
     year = db.Column(db.Integer)
     poster_url = db.Column(db.String(200))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
