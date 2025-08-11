357499f4ae8fbaf6ab3e09ea38b6963ec3619c70
pepe <p.zajonz@posteo.de>
2025-08-11 05:41:14 +0200

    added params

--- movie_data_fetcher.py
+++ movie_data_fetcher.py
@@ -2,19 +2,28 @@ import requests
 import os
 from dotenv import load_dotenv
 from models import Movie
-
+from data_manager import DataManager
 
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
+BASE_URL = "http://www.omdbapi.com/"
 
-def fetch_data(movie_search):
+def fetch_data(title:str, year:str = None):
     """
     Fetches movie information from API.
     Returns a dictionary with the requested movie information.
     """
-    api_url = f'http://www.omdbapi.com/?t={movie_search}'
-    response = requests.get(api_url, API_KEY)
+    params = {
+        'apikey':API_KEY,
+        't':title,
+        'type':'movie'
+        }
+
+    if year:
+        params['y']=year
+
+    response = requests.get(BASE_URL, params=params)
     if response.status_code == requests.codes.ok:
         data = response.json()
         if data.get('Response') == 'True':
@@ -25,8 +34,3 @@ def fetch_data(movie_search):
     else:
         raise Exception(f"API Error: {response.status_code} - {response.text}")
 
-
-def get_information_for_all_movies(movielist):
-    movie_object_list = []
-    for movie in movielist:
-        movie = Movie()
